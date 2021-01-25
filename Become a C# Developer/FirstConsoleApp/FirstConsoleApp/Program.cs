e0d960a70591687911f262b84ffc01c032df036c
Emmanuel Olatunde <56281174+Emmanuel@users.noreply.github.com>
2021-01-25 09:34:12 -0500

    Created new folder to create API using ASP.NET CORE

--- Become a C# Developer/FirstConsoleApp/FirstConsoleApp/Program.cs
+++ Become a C# Developer/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -8,8 +8,17 @@ namespace FirstConsoleApp
 {
     class Program
     {
+        public static string title = "C# Essential Training";
+        public static int whichPart;
+
         static void Main(string[] args)
         {
+            whichPart = 2;
+            var message = string.Format("Welcome to {0} Part {1}!",title, whichPart);
+            Console.WriteLine(message);
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            
         }
     }
 }

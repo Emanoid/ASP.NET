886d4af8bf41a65a9c05debb24ff02f93d2afbb6
Emmanuel Olatunde <56281174+Emmanuel@users.noreply.github.com>
2021-01-26 11:58:54 -0500

    Created Resident API

---
+++ Create Web API ASP_NET CORE/ResidentApi/ResidentApi/Models/ResidentContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ResidentApi.Models
+{
+    public class ResidentContext : DbContext
+    {
+        public ResidentContext(DbContextOptions<ResidentContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Resident> Residents { get; set; }
+    }
+}

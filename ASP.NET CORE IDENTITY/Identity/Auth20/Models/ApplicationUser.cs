d34b9e0f3baf16e471e42ba781bb57b62cb93c8d
Emmanuel Olatunde <56281174+Emmanuel@users.noreply.github.com>
2021-02-25 16:45:00 -0500

    Started learning ASP.NET CORE IDENTITY

---
+++ ASP.NET CORE IDENTITY/Identity/Auth20/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth20.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public string Fullname { get; set; }
+        public int Age { get; set; }
+    }
+}

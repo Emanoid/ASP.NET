6e289708d76eea1bf43ad1bd206b553309c5e7a5
Emmanuel Olatunde <56281174+Emmanuel@users.noreply.github.com>
2021-03-23 15:24:52 -0400

    ASP.NET MVC Keycloak Client

---
+++ ASP.NET CORE IDENTITY/Identity/Auth20/Data/Migrations/20210302174129_IdentityModified1.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Auth20.Data.Migrations
+{
+    public partial class IdentityModified1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                newName: "IdentityUser");
+
+            migrationBuilder.RenameColumn(
+                name: "Fullname",
+                table: "IdentityUser",
+                newName: "FullName");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "IdentityUser",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "IdentityUser",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_IdentityUser",
+                table: "IdentityUser",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_IdentityUser_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "IdentityUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_IdentityUser_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "IdentityUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_IdentityUser_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "IdentityUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_IdentityUser_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "IdentityUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_IdentityUser_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_IdentityUser_UserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_IdentityUser_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_IdentityUser_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_IdentityUser",
+                table: "IdentityUser");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "IdentityUser");
+
+            migrationBuilder.RenameTable(
+                name: "IdentityUser",
+                newName: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "AspNetUsers",
+                newName: "Fullname");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "AspNetUsers",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Keycloak Client/MVC_Keycloak/MVC_Keycloak/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Owin.Security.Keycloak;
+
+[assembly: OwinStartup(typeof(MVC_Keycloak.Startup))]
+
+namespace MVC_Keycloak
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Name of the persistent authentication middleware for lookup
+            const string persistentAuthType = "MVC_Keycloak_cookie_auth";
+
+            // --- Cookie Authentication Middleware - Persists user sessions between requests
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = persistentAuthType
+            });
+            app.SetDefaultSignInAsAuthenticationType(persistentAuthType); // Cookie is primary session store
+
+            // --- Keycloak Authentication Middleware - Connects to central Keycloak database
+            app.UseKeycloakAuthentication(new KeycloakAuthenticationOptions
+            {
+                // App-Specific Settings
+                ClientId = "keycloak-asp-net", // *Required*
+                ClientSecret = "7681018a-f8c6-4d3e-bc06-5a84f079a7e3", // If using public authentication, delete this line
+                VirtualDirectory = "", // Set this if you use a virtual directory when deploying to IIS
+
+                // Instance-Specific Settings
+                Realm = "Realm1", // Don't change this unless told to do so
+                KeycloakUrl = "http://localhost:8180/auth", // Enter your Keycloak URL here
+
+                // Template-Specific Settings
+                SignInAsAuthenticationType = persistentAuthType, // Sets the above cookie with the Keycloak data
+                AuthenticationType = "KeycloakOwinAuthenticationSample_keycloak_auth", // Unique identifier for the auth middleware
+            });
+        }
+    }
+}
+//https://github.com/dylanplecki/KeycloakOwinAuthentication/wiki/ASP.NET-MVC-Tutorial

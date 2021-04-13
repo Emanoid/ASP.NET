24b61c7d3dc9ec3569ef31859c1af1015c1d5fcc
Emmanuel Olatunde <56281174+Emmanuel@users.noreply.github.com>
2021-04-13 12:17:56 -0400

    new updates

--- Intro to Web API/MVCDATAFromDatabasetoFrom/MVCApp/Controllers/HomeController.cs
+++ Intro to Web API/MVCDATAFromDatabasetoFrom/MVCApp/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MVCApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataLibrary;
+using DataLibrary.BusinessLogic;
 
 namespace MVCApp.Controllers
 {
@@ -23,6 +26,48 @@ namespace MVCApp.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewEmployees()
+        {
+            ViewBag.Message = "Employees List";
+
+            var data = EmployeeProcessor.LoadEmployees();
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            foreach (var row in data)
+            {
+                employees.Add(new EmployeeModel
+                {
+                    EmployeeId = row.EmployeeId,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress,
+                    ConfirmEmail = row.EmailAddress
+                });
+            }
+
+            return View(employees);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(EmployeeModel model)
+        {
+            if (ModelState.IsValid) 
+            {
+                int recordsCreated = EmployeeProcessor.CreateEmployee(model.EmployeeId, model.FirstName, model.LastName, model.EmailAddress);
+                return RedirectToAction("Index");
+            }
 
             return View();
         }
---
+++ Intro to Web API/MVCDATAFromDatabasetoFrom/MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name = "Employee ID")]
+        [Range(100000, 999999, ErrorMessage ="You need to enter a valid EmployeeId")]
+        public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "You need to enter your first name.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You need to enter your last name.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "You need to enter your email address.")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email")]
+        [DataType(DataType.EmailAddress)]
+        [Compare("EmailAddress", ErrorMessage = "The Email and the Confirm Email must match.")]
+        public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "You must have a password.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength =10, ErrorMessage = "You need to provide a long enough password.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "You must re-enter your password.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The Password and the Confirm Password do not match.")]
+        public string Confirmpassword { get; set; }
+    }
+}
